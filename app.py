525ce868be51cb7c2c1f2241cd416f757e942903
Jared Coleman <jaredraycoleman@gmail.com>
2023-10-13 15:58:00 -0700

    Updates content

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+"""Main application file for the website."""
+import os
+
 from flask import Flask, render_template
 from flask_talisman import Talisman
-import os
-from data import pubs, projects as projs, websites
+
+from data import projects as projs
+from data import pubs, websites
 
 app = Flask(__name__)
 
@@ -10,18 +14,22 @@ if os.getenv('FLASK_ENV') == 'production':
 
 @app.route('/')
 def home():
+    """Renders the home page."""
     return render_template('index.html')
 
 @app.route('/publications')
 def publications():
+    """Renders the publications page."""
     return render_template('publications.html', publications=pubs, websites=websites)
 
 @app.route('/projects')
 def projects():
+    """Renders the projects page."""
     return render_template('projects.html', projects=projs)
 
 @app.route('/about')
 def about():
+    """Renders the about page."""
     return render_template('about.html')
 
 if __name__ == "__main__":
